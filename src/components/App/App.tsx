9290db210b221f24547210baf8f73a8cff138e5f
LEITTO <xxleittoxx@gmail.com>
2025-08-30 14:27:05 +0200

    additional styling and responsiveness

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -1,12 +1,12 @@
 import LandingPage from "../LandingPage/LandingPage";
-import "./App.module.css";
+import css from "./App.module.css";
 
 function App() {
-    return (
-        <div className="app">
-            <LandingPage />
-        </div>
-    );
+  return (
+    <div className={css.container}>
+      <LandingPage />
+    </div>
+  );
 }
 
 export default App;

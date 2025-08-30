3a3cba3db28c451915362b1048efc33597dd5122
Stanislav Nemchenko <stas.buka@gmail.com>
2025-08-31 01:06:01 +0300

    Header menu done and LandingPage

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,5 +1,34 @@
+import React from "react";
+import { GoTrophy } from "react-icons/go";
+import { RiLogoutCircleRLine } from "react-icons/ri";
 import css from "./Header.module.css";
 
-export default function Header() {
-    return <></>;
+interface HeaderProps {
+    userName: string;
+}
+
+export default function Header({ userName }: HeaderProps) {
+    const userInitial = userName ? userName.charAt(0).toUpperCase() : "";
+
+    return (
+        <header className={css.headerContainer}>
+            <div className={css.logo}>Questify</div>
+            <div className={css.userInfo}>
+                <div className={css.userAvatar}>{userInitial}</div>
+                <div className={css.userName}>{userName}'s Quest Log</div>
+            </div>
+            <div className={css.actions}>
+                <button className={css.actionButton}>
+                    <div className={css.trophyButtonWrapper}>
+                        {" "}
+                        {/* Обертка для иконки Challenge */}
+                        <GoTrophy className={css.trophyIcon} />
+                    </div>
+                </button>
+                <button className={css.actionButton}>
+                    <RiLogoutCircleRLine className={css.logoutIcon} />
+                </button>
+            </div>
+        </header>
+    );
 }

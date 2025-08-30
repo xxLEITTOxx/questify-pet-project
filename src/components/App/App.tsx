d50e08e37c42873661d2e6f7c1bfc5e2988245b0
LEITTO <xxleittoxx@gmail.com>
2025-08-30 22:21:37 +0200

    uncommenting section

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -1,18 +1,15 @@
-// import LandingPage from "../LandingPage/LandingPage";
+import LandingPage from "../LandingPage/LandingPage";
 // import QuestCard from "../QuestCard/QuestCard";
 import QuestDashboard from "../QuestDashboard/QuestDashboard";
 import css from "./App.module.css";
 
 function App() {
-
-    return (
-        <div className={css.container}>
-            <LandingPage />
-            
-            <QuestDashboard />
-        </div>
-    );
-
+  return (
+    <div className={css.container}>
+      <LandingPage />
+      <QuestDashboard />
+    </div>
+  );
 }
 
 export default App;

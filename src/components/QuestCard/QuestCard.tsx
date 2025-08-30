342b824c7bc98c82fba7669859bd83c77e740b1c
LEITTO <xxleittoxx@gmail.com>
2025-08-30 15:09:38 +0200

    common styles

--- src/components/QuestCard/QuestCard.tsx
+++ src/components/QuestCard/QuestCard.tsx
@@ -1,1 +1,23 @@
 // Карточка для отдельного квеста
+import css from "./QuestCard.module.css";
+import { MdOutlineClear, MdDone, MdOutlineSave } from "react-icons/md";
+
+export default function QuestCard() {
+  return (
+    <div className={css.cardContainer}>
+      <h1>Quest Card</h1>
+      {/* <div className={css.closeIcon}></div> */}
+      <ul>
+        <li>
+          <MdOutlineClear />
+        </li>
+        <li>
+          <MdDone />
+        </li>
+        <li>
+          <MdOutlineSave />
+        </li>
+      </ul>
+    </div>
+  );
+}

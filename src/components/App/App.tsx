b20a4727b25137b95c8ae534294cedfd8ad9e015
Stanislav Nemchenko <stas.buka@gmail.com>
2025-08-30 22:49:07 +0300

    landing page

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -3,12 +3,12 @@ import QuestCard from "../QuestCard/QuestCard";
 import css from "./App.module.css";
 
 function App() {
-  return (
-    <div className={css.container}>
-      {/* <LandingPage /> */}
-      <QuestCard />
-    </div>
-  );
+    return (
+        <div className={css.container}>
+            <LandingPage />
+            {/* <QuestCard /> */}
+        </div>
+    );
 }
 
 export default App;

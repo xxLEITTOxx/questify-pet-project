bcec7540749727b3bae300a77269b802c6c22b22
Stanislav Nemchenko <stas.buka@gmail.com>
2025-08-31 22:49:31 +0300

    done 50% PromoQuestCards

--- src/components/QuestCard/QuestCard.tsx
+++ src/components/QuestCard/QuestCard.tsx
@@ -1,54 +1,43 @@
-// Карточка для отдельного квеста
 import css from "./QuestCard.module.css";
-import {
-  MdOutlineClear,
-  //   MdDone,
-  //   MdOutlineSave,
-  MdOutlineStar,
-  MdArrowDropDown,
-} from "react-icons/md";
+import { MdOutlineClear, MdOutlineStar, MdArrowDropDown } from "react-icons/md";
 
-export default function QuestCard() {
-  return (
-    <div className={css.cardContainer}>
-      <div className={css.cardHeader}>
-        <div className={css.cardHeaderSelector}>
-          <div className={css.roundLevelSelector}></div>
-          <div className={css.levelTitle}>Normal</div>
-          <MdArrowDropDown />
+interface QuestCardProps {
+    level: string;
+    title: string;
+    category: string;
+}
+
+export default function QuestCard({ level, title, category }: QuestCardProps) {
+    return (
+        <div className={css.cardContainer}>
+            <div className={css.cardHeader}>
+                <div className={css.cardHeaderSelector}>
+                    <div
+                        className={`${css.roundLevelSelector} ${css[level]}`}
+                    ></div>
+                    <div className={css.levelTitle}>{level}</div>
+                    <MdArrowDropDown />
+                </div>
+                <div>
+                    <MdOutlineStar />
+                </div>
+            </div>
+            <div className={css.inputContainer}>
+                <div className={css.cardTitle}>{title}</div>
+                <input type="text" className={css.cardInput} />
+            </div>
+            <div className={css.cardBottomContainer}>
+                <div className={css.categorySelector}>
+                    <div>{category}</div>
+                    <MdArrowDropDown />
+                </div>
+                <div className={css.buttonList}>
+                    <div className={css.clearButton}>
+                        <MdOutlineClear />
+                    </div>
+                    <div className={css.startButton}>START</div>
+                </div>
+            </div>
         </div>
-        <div>
-          <MdOutlineStar />
-        </div>
-      </div>
-      <div className={css.inputContainer}>
-        <div className={css.cardTitle}>Create New Quest</div>
-        <input type="text" className={css.cardInput} />
-      </div>
-      <div className={css.cardBottomContainer}>
-        <div className={css.categorySelector}>
-          <div>category</div>
-          <MdArrowDropDown />
-        </div>
-        <div className={css.buttonList}>
-          <div className={css.clearButton}>
-            <MdOutlineClear />
-          </div>
-          <div className={css.startButton}>START</div>
-        </div>
-      </div>
-      {/* <div className={css.closeIcon}></div> */}
-      {/* <ul>
-        <li>
-          <MdOutlineClear />
-        </li>
-        <li>
-          <MdDone />
-        </li>
-        <li>
-          <MdOutlineSave />
-        </li>
-      </ul> */}
-    </div>
-  );
+    );
 }

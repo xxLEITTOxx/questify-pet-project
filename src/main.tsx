13e51efafef34c7484a83520462ffd892c6a3334
Stanislav Nemchenko <stas.buka@gmail.com>
2025-08-31 03:10:02 +0300

    added authorization, API

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,15 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
+import { Toaster } from "react-hot-toast";
 import "./index.css";
 import App from "./components/App/App";
+import { AuthProvider } from "./components/context/AuthContext";
 
 createRoot(document.getElementById("root")!).render(
     <StrictMode>
-        <App />
+        <AuthProvider>
+            <App />
+        </AuthProvider>
+        <Toaster />
     </StrictMode>
 );
